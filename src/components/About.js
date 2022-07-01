72d44e5c6b9dc16d319394fc1bf3ee6b5edd6df8
daniarfadd <daniarfadaffa@yahoo.com>
2022-07-01 11:30:42 +0700

    completed all the section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,10 @@
 function About() {
     return (  
-        <div>
+        <div className="about--section">
             <h2>About myself</h2>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <h2>Interests</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 function Footer() {
-    return ( 
-        <div>
-            <h4>My footer</h4>
-        </div>
-     );
+    return (
+        <footer>
+            <a href="https://github.com/daniarfadd">
+                <img src="../assets/github.png" />
+            </a>
+            <a href="https://www.facebook.com/daffa.daniarfa">
+                <img src="../assets/facebook.png" />
+            </a>
+            <a href="https://www.instagram.com/daffadaniarfa/">
+                <img src="../assets/instagram.png" />
+            </a>
+            <a href="https://twitter.com/Daffa_D8/">
+                <img src="../assets/twitter.png" />
+            </a>
+
+        </footer>
+    );
 }
 
 export default Footer;
